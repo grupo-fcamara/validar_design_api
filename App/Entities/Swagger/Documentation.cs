3de1b7aa83a7b74df1cdb0496f7c09f6d9ea936f
Raphael Fleury <raphael.fleury9@gmail.com>
2021-01-20 22:50:41 -0300

    Expanding Documentation class

--- App/Entities/Swagger/Documentation.cs
+++ App/Entities/Swagger/Documentation.cs
@@ -2,13 +2,39 @@ using System.Collections.Generic;
 
 namespace App.Entities.Swagger
 {
-    public class Documentation
+    public class Documentation : IDocumentation
     {
-        public string OpenAPI { get; set; }
-        public Dictionary<string, Path> Paths { get; set; }
+        public string Swagger { get; set; }
+        public Info Info { get; set; }
+
+        public string Host { get; set; }
+        public string BasePath { get; set; }
+        public string[] Schemes { get; set; }
+
+        public string[] Consumes { get; set; }
+        public string[] Produces { get; set; }
+
+        public Dictionary<string, Dictionary<string, Operation>> Paths { get; set; }
     }
 
-    public class Path
+    public class Info
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class Operation
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string[] Produces { get; set; }
+        public string[] Consumes { get; set; }
+        public Dictionary<int, Response> Responses { get; set; }
+        public bool Deprecated { get; set; }
+    }
+
+    public class Response
     {
 
     }
