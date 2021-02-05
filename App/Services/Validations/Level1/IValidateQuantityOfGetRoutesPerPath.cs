c26295b40d5b7c7734b120fe702472de3d2d9376
raphael-fleury <raphael.fleury9@gmail.com>
2021-02-05 18:00:30 -0300

    Refactor and bug fix

--- App/Services/Validations/Level1/IValidateQuantityOfGetRoutesPerPath.cs
+++
@@ -1,6 +0,0 @@
-using App.Entities.Swagger;
-
-namespace App.Services.Validations.Level1
-{
-    public interface IValidateQuantityOfGetRoutesPerPath : Generic.IValidator { }
-}
--- App/Services/Validations/Level1/ValidateGetRoutesPerPath.cs
+++ App/Services/Validations/Level1/ValidateGetRoutesPerPath.cs
@@ -10,17 +10,16 @@ namespace App.Services.Validations.Level1
         {
             var output = new ValidationOutput();
 
-            var completePaths = documentation.Paths;
-            var getFirstWord = completePaths.GroupBy(forEachPatch => new ApiPath(forEachPatch.Key).Pieces[0]);
-            getFirstWord = getFirstWord.OrderBy(paths => paths.Count(path => path.Value.Get != null));
+            var groupedByFirstWord = documentation.Paths
+                .GroupBy(path => new ApiPath(path.Key).Pieces[0])
+                .OrderBy(paths => paths.Count(path => path.Value.Get != null));
 
-            foreach (var paths in getFirstWord)
+            foreach (var paths in groupedByFirstWord)
             {
                 int GetQuantityOfGetsForEachPath = paths.Count(path => path.Value.Get != null);  
                 
-                if(GetQuantityOfGetsForEachPath > 2) {
-                    
-                    output.AddProblem($"Multiple get routes in path: {paths.Key}, quantity: {GetQuantityOfGetsForEachPath}, max per path: 2");    
+                if(GetQuantityOfGetsForEachPath > 2) {                      
+                    output.AddProblem($"{paths.Key} has {GetQuantityOfGetsForEachPath} GET routes, the maximum is 2.");    
                 }
             }
             return output;
