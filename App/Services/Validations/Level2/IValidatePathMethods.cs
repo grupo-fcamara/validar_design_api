7ad237a9044fe3404cf33034328b71ae953931fd
Raphael Fleury <raphael.fleury9@gmail.com>
2021-02-12 00:55:19 -0300

    Verifying if paths have operations

--- App/Services/Validations/Level2/IValidatePathMethods.cs
+++
@@ -1,9 +0,0 @@
-using App.Entities.Swagger;
-
-namespace App.Services.Validations.Level2
-{
-    public interface IValidatePathMethods : Generic.IValidator
-    {
-        string Path { get; }
-    }
-}
--- Tests/Services/Validations/PathLevelsValidate.cs
+++ Tests/Services/Validations/PathLevelsValidate.cs
@@ -1,28 +1,16 @@
 using Xunit;
 using System.Linq;
-using System.Collections.Generic;
-using App.Entities.Swagger;
 using App.Services.Validations.Level1;
 
 namespace Tests
 {
-    public class PathLevelsValidate
+    public class PathLevelsValidate : ValidatePaths
     {
         [Theory]
         [InlineData(2, 2, "users/{id}/pets/{id}", "countries/{id}/teams/{id}/players", "countries/{id}/teams/{id}/players/{id}")]
         public void ReturnProperly(int expectedProblems, int levelsLimit, params string[] paths)
         {
-            var doc = new Documentation();
-            doc.Paths = new Dictionary<string, SwaggerPathItem>();
-            
-            foreach (var path in paths)
-            {
-                doc.Paths[path] = new SwaggerPathItem();
-            }
-
-            var validator = new ValidatePathLevels(levelsLimit);
-            var output = validator.Validate(doc);
-
+            var output = ReturnProblems(new ValidatePathLevels(levelsLimit), paths);
             Assert.Equal(expectedProblems, output.Problems.Count());
         }
     }
