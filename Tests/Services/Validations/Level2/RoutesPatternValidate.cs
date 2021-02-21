9fa9740938cf409ac906aa9d593c905b7c550a35
raphael-fleury <raphael.fleury9@gmail.com>
2021-02-21 00:50:12 -0300

    Created test for Routes pattern validation

---
+++ Tests/Services/Validations/Level2/RoutesPatternValidate.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using System.Linq;
+using App.Services.Validations.Level2;
+using App.Entities;
+
+namespace Tests.Services.Validations.Level2
+{
+    public class RoutesPatternValidate : ValidatePaths
+    {
+        [Theory]
+        [InlineData(2, CASE.CAMEL, true,
+            "users/{id}/pets/{id}", 
+            "users/add/{id}", 
+            "user/add/{id}", 
+            "usuario/adicionar/{id}"
+        )]
+        public void ReturnProperly(int expectedProblems, CASE casePattern, bool plural, params string[] paths)
+        {
+            var output = ReturnProblems(new ValidateRoutesPattern(casePattern, plural), paths);
+            Assert.Equal(expectedProblems, output.Problems.Count());
+        }
+    }
+}
