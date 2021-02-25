6c5c1e3868cb410d65ee3b9b9fb84dcb7fef339a
Raphael Fleury <raphael.fleury9@gmail.com>
2021-02-24 22:20:01 -0300

    Fixing NullException

--- Tests/Services/Validations/Level3/StatusCodeValidate.cs
+++ Tests/Services/Validations/Level3/StatusCodeValidate.cs
@@ -8,28 +8,35 @@ using App.Entities.Swagger;
 
 namespace Tests.Services.Validations.Level3
 {
-    public class StatusCodeValidate
+    public class StatusCodeValidate : Validation
     {
         [Fact]
         public void ReturnProperly()
-        {            
-            var documentation = new DocumentationForTests();
+        {
             var endPoints = new List<EndPoint>();
-
-            endPoints.Add(new EndPoint() { Verb = HTTPVERBS.GET, Responses = new int[] { 200, 404 } });
-            endPoints.Add(new EndPoint() { Verb = HTTPVERBS.GET, Responses = new int[] { 200, 500 } });
-            endPoints.Add(new EndPoint() { Verb = HTTPVERBS.POST, Responses = new int[] { 200 } });
-            endPoints.Add(new EndPoint() { Verb = HTTPVERBS.POST, Responses = new int[] { 200, 500, 404 } });
-            endPoints.Add(new EndPoint() { Verb = HTTPVERBS.PUT, Responses = new int[] { } });
-            documentation.EndPoints = endPoints.ToArray();
+            endPoints.Add(CreateEndPoint("1", HTTPVERBS.GET, 200, 404));
+            endPoints.Add(CreateEndPoint("2", HTTPVERBS.GET, 200, 500));
+            endPoints.Add(CreateEndPoint("3", HTTPVERBS.POST, 200));
+            endPoints.Add(CreateEndPoint("4", HTTPVERBS.POST, 200, 500, 404));
+            endPoints.Add(CreateEndPoint("5", HTTPVERBS.PUT));
 
             var allowedCodes = StatusCodePerVerb.Empty;
             allowedCodes[HTTPVERBS.GET] = new int[] {200, 404};
             allowedCodes[HTTPVERBS.POST] = new int[] {200, 500};
             allowedCodes[HTTPVERBS.PUT] = new int[] {300, 504};
 
-            var output = new ValidateStatusCode(StatusCodePerVerb.Empty).Validate(documentation);
+            var output = ReturnProblems(new ValidateStatusCode(allowedCodes), endPoints.ToArray());
             Assert.Equal(2, output.Problems.Count());
+        }
+
+        private EndPoint CreateEndPoint(string path, HTTPVERBS verb, params int[] responses)
+        {
+            return new EndPoint()
+            {
+                Path = new ApiPath(path),
+                Verb = verb,
+                Responses = responses
+            };
         }
     }
 }
